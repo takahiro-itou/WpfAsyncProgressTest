473a3e98756e1b239a3b243f813e93d637c8cf3c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-06-18 19:59:58 +0900

    プログレスのインスタンスを生成

--- WpfAsyncProgressTest/ViewModels/ProgressSampleViewModel.cs
+++ WpfAsyncProgressTest/ViewModels/ProgressSampleViewModel.cs
@@ -4,6 +4,17 @@ namespace WpfAsyncProgressTest
 
 public class ProgressSampleViewModel
 {
+    private readonly    IProgress<int>  m_progress;
+
+    public ProgressSampleViewModel()
+    {
+        this.m_progress = new Progress<int>(ProgressChanged);
+    }
+
+    public void ProgressChanged(int progressValue)
+    {
+    }
+
 }
 
 }
