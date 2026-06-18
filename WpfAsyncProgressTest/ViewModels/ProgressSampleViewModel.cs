3cfb2890fb6b57e9fba2abe1c4369ed48bbf7dd5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-06-18 20:06:56 +0900

    重たい処理の例をビューモデルにコピー

--- WpfAsyncProgressTest/ViewModels/ProgressSampleViewModel.cs
+++ WpfAsyncProgressTest/ViewModels/ProgressSampleViewModel.cs
@@ -23,6 +23,19 @@ public class ProgressSampleViewModel
         set { this.m_progressValue = value; }
     }
 
+    public  int  HeavyTask()
+    {
+        int total = 0;
+
+        this.m_progress.Report(0);
+        for ( int i = 1; i <= 20; ++ i ) {
+            total += i;
+            Thread.Sleep(1000);
+            this.m_progress.Report(i * 5);
+        }
+        return ( total );
+    }
+
 }
 
 }
