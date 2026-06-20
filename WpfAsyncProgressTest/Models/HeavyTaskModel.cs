0bbad584de40430f5aaaf02405746b9cda398358
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-06-20 10:17:19 +0900

    リファクタリング

--- WpfAsyncProgressTest/Models/HeavyTaskModel.cs
+++ WpfAsyncProgressTest/Models/HeavyTaskModel.cs
@@ -4,6 +4,22 @@ namespace WpfAsyncProgressTest.Models
 
 public class HeavyTaskModel
 {
+    public  int
+    doHeavyTask(
+        IProgress<int>  progress)
+    {
+        int total = 0;
+
+        progress.Report(0);
+        for ( int i = 1; i <= 20; ++ i ) {
+            total += i;
+            Thread.Sleep(1000);
+            progress.Report(i * 5);
+        }
+
+        return ( total );
+    }
+
 }
 
 }
